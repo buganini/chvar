325f1d117393880d14928d5d3063e4e367732069
Buganini <buganini@security-hole.info>
2009-12-21 04:07:46 +0800

    add web interface in use.

---
+++ web/chvar.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.1.41, for portbld-freebsd8.0 (i386)
+--
+-- Host: localhost    Database: chvar
+-- ------------------------------------------------------
+-- Server version	5.1.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `axiom`
+--
+
+DROP TABLE IF EXISTS `axiom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `axiom` (
+  `axiom` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `data`
+--
+
+DROP TABLE IF EXISTS `data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data` (
+  `master` varchar(255) DEFAULT NULL,
+  `slave` varchar(255) DEFAULT NULL,
+  `ctime` double DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2009-12-21  4:03:51
+
+INSERT INTO `axiom` values (1);
---
+++ web/engine.php
@@ -0,0 +1,68 @@
+<?php
+$func='chvar_'.$_REQUEST['action'];
+if(function_exists($func)){
+	$db=new mysqli('localhost','chvar','idiot','chvar');
+	$db->autocommit(true);
+	$db->query('SET NAMES UTF8');
+	$func();
+}
+
+function hexval($s){
+	$s=preg_replace('/[^0-9a-f]/si','',$s);
+	$s=ltrim($s,'0');
+	return strtoupper($s);
+}
+
+function mctime(){
+	$t=explode(' ',microtime());
+	return $t[0]+$t[1];
+}
+
+function chvar_add(){
+	global $db;
+	$a=explode("\n",trim($_POST['text']));
+	$a[0]=hexval($a[0]);
+	if($a[0]){
+		for($i=1;$i<count($a);++$i){
+			$a[$i]=hexval($a[$i]);
+			if($a[$i]){
+				$sql=$db->prepare('INSERT INTO `data` (`master`,`slave`,`ctime`) SELECT ?,?,? FROM `axiom` WHERE NOT EXISTS (SELECT 1 FROM `data` WHERE `master`=? AND `slave`=? LIMIT 1)');
+				$sql->bind_param('ssdss',$a[0],$a[$i],mctime(),$a[0],$a[$i]);
+				$sql->execute();
+			}
+		}
+	}
+	chvar_fetch();
+}
+
+function f($s){
+	$s=hexval($s);
+	if(strlen($s) % 2){
+		return '010'.$s;
+	}
+	return '01'.$s;
+}
+
+function chvar_fetch(){
+	global $db;
+	$sql=$db->prepare('SELECT `master`,`slave`,`ctime` FROM `data` WHERE `ctime` > ? ORDER BY `ctime` ASC LIMIT 25');
+	$sql->bind_param('d',$_REQUEST['last']);
+	$sql->execute();
+	$sql->bind_result($m,$s,$c);
+	$data=array();
+	$cv=bsdconv_create('bsdconv:utf-8,ascii');
+	while($sql->fetch()){
+		$data[]=array($m,$s,$c,bsdconv($cv,f($m)),bsdconv($cv,f($s)));
+	}
+	bsdconv_destroy($cv);
+	echo json_encode($data);
+}
+
+function chvar_dump(){
+	global $db;
+	$res=$db->query('SELECT * FROM `data` ORDER BY `ctime`');
+	while($r=$res->fetch_assoc()){
+		echo $r['master']."\t".$r['slave']."\n";
+	}
+}
+?>
