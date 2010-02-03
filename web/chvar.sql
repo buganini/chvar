f35aba0ad487de75616579126fdf47a10a493853
Buganini <buganini@security-hole.info>
2010-02-03 15:15:26 +0800

    add attribute table

--- web/chvar.sql
+++ web/chvar.sql
@@ -16,6 +16,42 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `attr1`
+--
+
+DROP TABLE IF EXISTS `attr1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attr1` (
+  `id` int(11) DEFAULT NULL,
+  `cn` varchar(255) DEFAULT NULL,
+  `tw` varchar(255) DEFAULT NULL,
+  `gb2312` varchar(255) DEFAULT NULL,
+  `cp950` varchar(255) DEFAULT NULL,
+  `cp936` varchar(255) DEFAULT NULL,
+  `gbk` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `attr2`
+--
+
+DROP TABLE IF EXISTS `attr2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `attr2` (
+  `id` int(11) DEFAULT NULL,
+  `cn` varchar(255) DEFAULT NULL,
+  `tw` varchar(255) DEFAULT NULL,
+  `gb2312` varchar(255) DEFAULT NULL,
+  `cp950` varchar(255) DEFAULT NULL,
+  `cp936` varchar(255) DEFAULT NULL,
+  `gbk` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `axiom`
 --
 
@@ -42,15 +78,41 @@ CREATE TABLE `data` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `group`
+-- Table structure for table `group1`
 --
 
-DROP TABLE IF EXISTS `group`;
+DROP TABLE IF EXISTS `group1`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `group` (
+CREATE TABLE `group1` (
   `id` int(11) DEFAULT NULL,
   `data` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `group2`
+--
+
+DROP TABLE IF EXISTS `group2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group2` (
+  `id` int(11) DEFAULT NULL,
+  `data` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `group3`
+--
+
+DROP TABLE IF EXISTS `group3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group3` (
+  `id` int(11) DEFAULT NULL,
+  `data` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -63,4 +125,6 @@ CREATE TABLE `group` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-01-30  8:33:14
+-- Dump completed on 2010-02-03 15:11:45
+
+INSERT INTO `axiom` values (1);
