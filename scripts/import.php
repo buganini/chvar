0478c4b2e832db39279eb9ef6f8dd2f58092bdff
Buganini Q <buganini@cnmc.tw>
2011-12-28 20:41:51 +0800

    implement script for importing attr map

--- scripts/import.php
+++ scripts/import.php
@@ -8,6 +8,21 @@ $db->autocommit(true);
 $db->query('SET NAMES UTF8');
 
 if($table=='attr1' || $table=='attr2'){
+	$fp=fopen($file,'r');
+	$i=0;
+	while(($line=fgets($fp))!==false){
+		$a=explode("\t",$line);
+		if($i==0){
+			for($j=0;$j<count($a);++$j)
+				$a[$j]='`'.trim($a[$j]).'`';
+			$fields=implode(',',$a);
+		}else{
+			for($j=0;$j<count($a);++$j)
+				$a[$j]='"'.trim($a[$j]).'"';
+			$db->query('INSERT INTO `'.$table.'` ('.$fields.') VALUES ('.implode(',',$a).')');
+		}
+		$i+=1;
+	}
 
 }elseif($table=='group1' || $table=='group2'){
 	$fp=fopen($file,'r');
