df30cfb61a8ec0866d871b089286929b8243c058
Buganini Q <buganini@gmail.com>
2012-04-02 15:04:08 +0800

    implement code for importing attr data back to db

--- scripts/import.php
+++ scripts/import.php
@@ -8,8 +8,27 @@ $db->autocommit(true);
 $db->query('SET NAMES UTF8');
 
 if($table=='attr1' || $table=='attr2'){
-
+	$db->query('DELETE FROM  `'.$table.'`');
+	$t=file_get_contents($file);
+	$a=explode("\n",trim($t));
+	$f=explode("\t",trim($a[0]));
+	foreach($f as $k=>$v){
+		$f[$k]='`'.strtolower($f[$k]).'`';
+	}
+	$fn=count($f);
+	$f=implode(',',$f);
+	for($i=1;$i<count($a);++$i){
+		$d=explode("\t",trim($a[$i]));
+		foreach($d as $k=>$v){
+			$d[$k]='"'.$d[$k].'"';
+		}
+		for(;$k<$fn;++$k){
+			$d[$k]='""';
+		}
+		$db->query('INSERT INTO `'.$table.'` ('.$f.') VALUES ('.implode(',',$d).')');
+	}
 }elseif($table=='group1' || $table=='group2'){
+	$db->query('DELETE FROM  `'.$table.'`');
 	$fp=fopen($file,'r');
 	while(($line=fgets($fp))!==false){
 		$a=explode("\t",$line);
@@ -19,6 +38,7 @@ if($table=='attr1' || $table=='attr2'){
 		$data=trim($a[1]);
 		$db->query('INSERT INTO `'.$table.'` (`id`,`data`) VALUES ("'.$id.'","'.$data.'")');
 	}
+	fclose($fp);
 }
 
 $db->close();
