ff6648e51629ab13be2170e37349b3c439a9ed4b
Buganini Q <buganini@gmail.com>
2015-08-22 14:31:58 +0800

    add query command

--- chvar/__main__.py
+++ chvar/__main__.py
@@ -9,6 +9,7 @@ def usage():
 	print("python chvar . transliterate {CP950,CP936,GB2312,GBK}\n")
 	print("python chvar . normalize {TW,CN}\n")
 	print("python chvar . fuzzy {TW,CN}\n")
+	print("python chvar . query {試,9644}\n")
 	sys.exit()
 
 attr1 = Attr(os.path.join(sys.argv[1], "attr1.txt"))
@@ -21,10 +22,15 @@ chvar = Chvar((group1, attr1), (group2, attr2))
 
 argv = {"transliterate":("CP950","CP936","GB2312","GBK"), "normalize":("TW","CN"), "fuzzy":("TW","CN")}
 
-if sys.argv[2] not in ("transliterate", "normalize", "fuzzy"):
-	usage()
+if sys.argv[2] == "query":
+	if len(sys.argv) < 4:
+		usage()
+	chvar.query(sys.argv[3])
+else:
+	if sys.argv[2] not in ("transliterate", "normalize", "fuzzy"):
+		usage()
 
-if sys.argv[3] not in argv[sys.argv[2]]:
-	usage()
+	if sys.argv[3] not in argv[sys.argv[2]]:
+		usage()
 
-chvar.dump(sys.argv[2], sys.argv[3])
+	chvar.dump(sys.argv[2], sys.argv[3])
--- chvar/chvar.py
+++ chvar/chvar.py
@@ -1,3 +1,5 @@
+import re
+import sys
 from bsdconv import Bsdconv
 from helper import *
 
@@ -47,3 +49,38 @@ class Chvar():
 			r = func(k, cat)
 			if r != k:
 				print("{0}\t{1}".format(k, r))
+
+	def query(self, key):
+		if re.match(r"^[0-9A-Fa-f]+$", key):
+			key = key.upper()
+		elif type(key) != type(u""):
+			key = key.decode("utf-8").encode("unicode_escape")[2:].upper()
+
+		g = self.layers[0][0].rdata.get(key)
+		if not g:
+			print("No Data.")
+			return
+
+		g2 = self.layers[1][0].rdata.get(g)
+		if not g2:
+			self._print_group1(g)
+			return
+
+		print("Layer 2 Group {0}".format(g2))
+		for g in self.layers[1][0].data.get(g2):
+			self._print_group1(g)
+
+	def _print_group1(self, g):
+		print("\tLayer 1 Group {0}".format(g))
+		print("\t\tMember:")
+		c = Bsdconv("bsdconv:utf-8")
+
+		d = self.layers[0][0].data.get(g)
+		for e in d:
+			print("\t\t\t{0} ({1})".format(c.conv(p01(e)), e))
+
+		a = self.layers[0][1].get(g)
+		print("\t\tAttributes:")
+		for cat in a:
+			print("\t\t\t{0}: {1} ({2})".format(cat, c.conv(p01(a[cat])), e))
+		print("")
