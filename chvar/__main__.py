b8de1205de1f44b90c163a5c1091a24ded5795d0
Buganini Q <buganini@gmail.com>
2017-11-13 22:31:21 +0800

    support JP in normaliza/fuzzy table generation; update readme

--- chvar/__main__.py
+++ chvar/__main__.py
@@ -7,8 +7,8 @@ from chvar import Chvar
 
 def usage():
 	print("python chvar . transliterate {CP950,CP936,GB2312,GBK}\n")
-	print("python chvar . normalize {TW,CN}\n")
-	print("python chvar . fuzzy {TW,CN}\n")
+	print("python chvar . normalize {TW,CN,JP}\n")
+	print("python chvar . fuzzy {TW,CN,JP}\n")
 	print("python chvar . query {試,9644}\n")
 	sys.exit()
 
@@ -23,7 +23,7 @@ group2 = Group(os.path.join(sys.argv[1], "group2.txt"))
 
 chvar = Chvar((group1, attr1), (group2, attr2))
 
-argv = {"transliterate":("CP950","CP936","GB2312","GBK"), "normalize":("TW","CN"), "fuzzy":("TW","CN")}
+argv = {"transliterate":("CP950","CP936","GB2312","GBK"), "normalize":("TW","CN","JP"), "fuzzy":("TW","CN","JP")}
 
 if sys.argv[2] == "query":
 	chvar.query(sys.argv[3])
