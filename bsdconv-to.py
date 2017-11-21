afb2b56a01239d909d049a734728a149758e25c8
Buganini Q <buganini@gmail.com>
2017-11-21 11:55:40 +0800

    bsdconv-to.py: fix with newest python-bsdconv

--- bsdconv-to.py
+++ bsdconv-to.py
@@ -4,11 +4,11 @@ import sys
 from chvar.helper import p01
 from bsdconv import Bsdconv
 
-c = Bsdconv("bsdconv:{0}".format(sys.argv[1]))
+c = Bsdconv("bsdconv:{0}|byte:hex".format(sys.argv[1]))
 
 for l in sys.stdin:
 	l = l.strip()
 	if not l:
 		continue
 	l = l.split("\t")
-	print("{0}\t{1}".format(p01(l[0]), c.conv(p01(l[1])).encode("hex").upper()))
+	print("{0}\t{1}".format(p01(l[0]), c.conv(p01(l[1])).decode("utf-8").upper()))
